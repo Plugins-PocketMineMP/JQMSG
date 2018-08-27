3d7f91d68d8c6fea831e0d2e5484319bad52b0a9
alvin0319 <32565818+alvin0319@users.noreply.github.com>
2018-08-27 17:59:59 +0900

    입장시 콘솔 로그 출력 및 오피 에게 입장로그 출력여부 부분 추가
    오피 입장 로그 여부는 콘피그에서 설정할수 있으며 기본 세팅은 false 이며 true 로 바꿀시 로그가 오피에게 갑니다
    
    콘솔은 무조건 로그가 나갑니다.
    
    PS. 기존 콘피그 파일을 지우고 다시 새로 하셔야 작동합니다.

--- JQMSG.php
+++ JQMSG.php
@@ -25,7 +25,9 @@ class JQMSG extends PluginBase implements Listener {
 		"USER-Join-msg" => "§e[ §f로그인§e ] §f{이름}",
 		"OP-Quit-msg" => "§c관리자§f {이름}§f님이 서버에서 퇴장하셨습니다",
 		"USER-Quit-msg" => "§e[ §f로그아웃§e ] §f{이름}",
-		"prefix" => "§a§l[ §f서버 §a] "
+		"prefix" => "§a§l[ §f서버 §a] ",
+		"No-has-player-data" => "{이름}님이 서버에 처음 접속하셨습니다!",
+		"OP-Logger" => false
 		]);
 		$this->db = $this->config->getAll();
 		$this->getServer()->getPluginManager()->registerEvents ($this, $this);
@@ -36,18 +38,39 @@ class JQMSG extends PluginBase implements Listener {
 	}
 	public function onJoin(PlayerJoinEvent $event) {
 		$event->setJoinMessage(false);
+		$count = count($this->getServer()->getOnlinePlayers());
+		$this->getServer()->getLogger()->info ("§e" . $event->getPlayer()->getName() . "님 입장\n현재인원: " . $count . "명");
 		if ($event->getPlayer()->isOp()) {
 			$this->getServer()->broadcastMessage ($this->db["prefix"] . $this->Replace($event->getPlayer()->getName(), $this->db["OP-Join-msg"]));
 		} else {
 			$this->getServer()->broadcastPopup ($this->Replace($event->getPlayer()->getName(), $this->db["USER-Join-msg"]));
 		}
+		if (! $event->getPlayer()->hasPlayedBefore()) {
+		    $this->getServer()->broadcastMessage ($this->db["prefix"] . $this->Replace($event->getPlayer()->getName(), $this->db["No-has-player-data"]));
+		}
+		if ($this->db["OP-Logger"] === true) {
+		    foreach ($this->getServer()->getOnlinePlayers() as $op) {
+		        if ($op->isOp()) {
+		            $op->sendMessage ("§e" . $event->getPlayer()->getName() . "입장\n현재인원: " . $count . "명");
+		        }
+		    }
+		}
 	}
 	public function onQuit(PlayerQuitEvent $event) {
 		$event->setQuitMessage(false);
+		$count = count($this->getServer()->getOnlinePlayers());
+		$this->getServer()->getLogger()->info ("§e" . $event->getPlayer()->getName() . "님 퇴장\n현재인원: " . $count . "명");
 		if ($event->getPlayer()->isOp()) {
 			$this->getServer()->broadcastMessage ($this->db["prefix"] . $this->Replace($event->getPlayer()->getName(), $this->db["OP-Quit-msg"]));
 		} else {
 			$this->getServer()->broadcastPopup ("§e[ §f로그아웃§e ] §f" . $this->Replace($event->getPlayer()->getName(), $this->db["USER-Quit-msg"]));
+		}
+		if ($this->db["OP-Logger"] === true) {
+		    foreach ($this->getServer()->getOnlinePlayers() as $op) {
+		        if ($op->isOp()) {
+		            $op->sendMessage ("§e" . $event->getPlayer()->getName() . "퇴장\n현재인원: " . $count . "명");
+		        }
+		    }
 		}
 	}
 }
